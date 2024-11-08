19d8748dc1ed0762b20e4dcc279e3947032f7950
Mikee55 <milkyasterefe@gmail.com>
2024-11-08 22:32:59 +0300

    update 3

---
+++ public/timer.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#circle-button-one").on('click', function() {
+        setInterval(() => {
+            console.log("button clicked");
+        }, 1000);
+
+        clearInterval
+        
+    });
+});
