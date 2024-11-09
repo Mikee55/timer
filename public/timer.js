ed749d0639d03fe8308b87fc75d2b1f01a8542ea
Mikee55 <milkyasterefe@gmail.com>
2024-11-10 01:13:30 +0300

    update 5

--- public/timer.js
+++ public/timer.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
             console.log("top button clicked");
             secondsBottom++;
-            if (secondsBottom === 10) {
+            if (secondsBottom === 60) {
                 minutesBottom++;
                 secondsBottom = 0;
             }
@@ -25,8 +25,23 @@ $(document).ready(function() {
         clearInterval(intervalIdTop);
 
         intervalIdBottom = setInterval(() => {
+            secondsTop++;
+            if (secondsTop === 60) {
+                minutesTop++;
+                secondsTop = 0;
+            }
             console.log("bottom button clicked");
             $('#top-timer').text(`${minutesTop.toString().padStart(2, '0')}:${secondsTop.toString().padStart(2, '0')}`);
         }, 1000);
+    });
+    $("#clear-button").on('click', function() {
+        clearInterval(intervalIdBottom);
+        clearInterval(intervalIdTop);
+        secondsTop = 0;
+        secondsBottom = 0;
+        minutesTop = 0;
+        minutesBottom = 0;
+        $('#bottom-timer').text(`${minutesBottom.toString().padStart(2, '0')}:${secondsBottom.toString().padStart(2, '0')}`);
+        $('#top-timer').text(`${minutesTop.toString().padStart(2, '0')}:${secondsTop.toString().padStart(2, '0')}`);
     });
 });
