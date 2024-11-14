7417cfc20d24b1bbc41f12ebf48b88ffe6a0c461
Mikee55 <milkyasterefe@gmail.com>
2024-11-14 20:38:32 +0300

    update 11

--- public/timer.js
+++ public/timer.js
@@ -2,21 +2,29 @@ $(document).ready(function() {
 
     let intervalIdTop;
     let intervalIdBottom;
-    let secondsTop = 0;
-    let secondsBottom = 0;
-    let minutesTop = 0;
-    let minutesBottom = 0;
+    let secondsTop = 60;
+    let secondsBottom = 60;
+    let minutesTop = 5;
+    let minutesBottom = 5;
+    let isTopTimerRunning = false;
+    let isBottomTimerRunning = false;
 
     $("#circle-button-one").on('click', function() {
         clearInterval(intervalIdBottom);
 
+        if (!isTopTimerRunning) {
+            minutesBottom--;
+            isTopTimerRunning = true;
+        }
+
         intervalIdTop = setInterval(() => {
 
             console.log("top button clicked");
-            secondsBottom++;
-            if (secondsBottom === 60) {
-                minutesBottom++;
-                secondsBottom = 0;
+            
+            secondsBottom--;
+            if (secondsBottom === 0) {
+                minutesBottom--;
+                secondsBottom = 60;
             }
             $('#bottom-timer').text(`${minutesBottom.toString().padStart(2, '0')}:${secondsBottom.toString().padStart(2, '0')}`);
         }, 1000);
@@ -24,11 +32,16 @@ $(document).ready(function() {
     $("#circle-button-two").on('click', function() {
         clearInterval(intervalIdTop);
 
+        if (!isBottomTimerRunning) {
+            minutesTop--;
+            isBottomTimerRunning = true;
+        }
+
         intervalIdBottom = setInterval(() => {
-            secondsTop++;
-            if (secondsTop === 60) {
-                minutesTop++;
-                secondsTop = 0;
+            secondsTop--;
+            if (secondsTop === 0) {
+                minutesTop--;
+                secondsTop = 60;
             }
             console.log("bottom button clicked");
             $('#top-timer').text(`${minutesTop.toString().padStart(2, '0')}:${secondsTop.toString().padStart(2, '0')}`);
@@ -37,11 +50,11 @@ $(document).ready(function() {
     $("#clear-button").on('click', function() {
         clearInterval(intervalIdBottom);
         clearInterval(intervalIdTop);
-        secondsTop = 0;
-        secondsBottom = 0;
-        minutesTop = 0;
-        minutesBottom = 0;
-        $('#bottom-timer').text(`${minutesBottom.toString().padStart(2, '0')}:${secondsBottom.toString().padStart(2, '0')}`);
-        $('#top-timer').text(`${minutesTop.toString().padStart(2, '0')}:${secondsTop.toString().padStart(2, '0')}`);
+        secondsTop = 60;
+        secondsBottom = 60;
+        minutesTop = 5;
+        minutesBottom = 5;
+        $('#bottom-timer').text(`${minutesBottom.toString().padStart(2, '0')}:00`);
+        $('#top-timer').text(`${minutesTop.toString().padStart(2, '0')}:00`);
     });
 });
